adbef4fdeb1488439ac0e6931eeb49cd05e245fd
Степан <stepandiduh6@gmail.com>
2022-06-12 14:17:12 +0300

    resolves, guards

--- src/app/models/IPostDetails.ts
+++
@@ -1,6 +0,0 @@
-import {IPost} from "./IPost";
-
-export interface IPostDetails extends IPost{
-  body: string,
-  title: string
-}
--- src/app/models/IUserDetails.ts
+++
@@ -1,6 +0,0 @@
-import {IUser} from "./IUser";
-
-export interface IUserDetails extends IUser{
-  email: string,
-  username: string
-}
